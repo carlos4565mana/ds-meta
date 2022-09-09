209d048b6b6e3c5f84b0a198586aef88d1ebe8e5
carlos4565mana <carloscal61@gmail.com>
2022-09-09 15:06:35 -0300

    Date select

--- backend/src/main/java/com/carlos/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/carlos/dsmeta/services/SaleService.java
@@ -2,8 +2,13 @@ package com.carlos.dsmeta.services;
 
 import com.carlos.dsmeta.entities.Sale;
 import com.carlos.dsmeta.repositories.SaleRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
@@ -14,7 +19,11 @@ public class SaleService {
         this.repository = repository;
     }
 
-    public List<Sale> findSales(){
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+        return repository.findSales(min, max, pageable);
     }
 }
