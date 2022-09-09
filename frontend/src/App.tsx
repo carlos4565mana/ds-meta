3d56c6eb76cf73fe4714a28c0cfca56e58f2c95b
carlos4565mana <carloscal61@gmail.com>
2022-09-09 12:26:43 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SallesCard from "./components/SallesCard"
 
 
 function App() {
 
   return (
-    <NotificationButton />
+    <>
+      <Header />
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SallesCard />
+        </div>
+      </section>
+    
+    </>
+    
   )
 }
 
